0f1859d2122a3e67be905038fbcfbfd996f1440e
Matheus Guerreiro <73855206+matheusguerreiro@users.noreply.github.com>
2022-06-22 21:36:26 -0300

    s3 a63

--- section 3: criando nossa primeira aplicação/crypto_wallet/db/seeds.rb
+++ section 3: criando nossa primeira aplicação/crypto_wallet/db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating Coins...'
+
+Coin.create!(
+  description: "Bitcoin",
+  acronym: "BTC",
+  url_image: "https://en.bitcoin.it/w/images/en/5/50/Bitcoin.png"
+)
+
+Coin.create!(
+  description: "Ethereum",
+  acronym: "ETH",
+  url_image: "https://s2.coinmarketcap.com/static/img/coins/200x200/2074.png"
+)
+
+Coin.create!(
+  description: "Dash",
+  acronym: "DASH",
+  url_image: "https://s2.coinmarketcap.com/static/img/coins/200x200/131.png"
+)
+
+puts 'Successfully created Coins'
