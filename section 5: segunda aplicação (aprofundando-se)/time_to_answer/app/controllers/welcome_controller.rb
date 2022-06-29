64427a21939098ef67a80e3fd0dfb0258d68cc6d
Matheus Guerreiro <73855206+matheusguerreiro@users.noreply.github.com>
2022-06-29 10:28:29 -0300

    s5 a130 Ajustando o sistema de layouts (Site)

--- section 5: segunda aplicação (aprofundando-se)/time_to_answer/app/controllers/welcome_controller.rb
+++
@@ -1,5 +0,0 @@
-class WelcomeController < ApplicationController
-  # http_basic_authenticate_with name: "eu", password: "12" - um exemplo de autenticação simples
-  def index
-  end
-end
--- section 5: segunda aplicação (aprofundando-se)/time_to_answer/config/routes.rb
+++ section 5: segunda aplicação (aprofundando-se)/time_to_answer/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :site do
+    get 'welcome/index'
+  end
   namespace :profiles_backoffice do
     get 'welcome/index'
   end
@@ -7,8 +10,7 @@ Rails.application.routes.draw do
   end
   devise_for :profiles
   devise_for :admins
-  get 'welcome/index'
-  get 'inicio', to: 'welcome#index'
-  root to: 'welcome#index' # para adicionar a primeira página no localhost:3000
+  get 'inicio', to: 'site/welcome#index'
+  root to: 'site/welcome#index' # para adicionar a primeira página no localhost:3000
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
