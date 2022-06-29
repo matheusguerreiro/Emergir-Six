2e32df1640b4c7623198e93640382f7519f6ee2a
Matheus Guerreiro <73855206+matheusguerreiro@users.noreply.github.com>
2022-06-29 12:21:30 -0300

    corrigido/reconstruído para users

--- section 5: segunda aplicação (aprofundando-se)/time_to_answer/config/routes.rb
+++ section 5: segunda aplicação (aprofundando-se)/time_to_answer/config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   namespace :site do
     get 'welcome/index'
   end
-  namespace :profiles_backoffice do
+  namespace :users_backoffice do
     get 'welcome/index'
   end
   namespace :admins_backoffice do
     get 'welcome/index'
   end
+  devise_for :users
   devise_for :admins
   get 'inicio', to: 'site/welcome#index'
   root to: 'site/welcome#index' # para adicionar a primeira página no localhost:3000
