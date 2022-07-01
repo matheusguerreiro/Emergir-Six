c66464827639bcd5fc77de2d6c666ccda778f1fb
Matheus Guerreiro <73855206+matheusguerreiro@users.noreply.github.com>
2022-07-01 13:06:11 -0300

    s5 a149. Ajustando a rota dos admins

--- section 5: segunda aplicação (aprofundando-se)/time_to_answer/config/routes.rb
+++ section 5: segunda aplicação (aprofundando-se)/time_to_answer/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  namespace :admins_backoffice do
-    get 'admins/index'
-  end
+  # namespace :admins_backoffice do
+  #   get 'admins/index'
+  #   get 'admins/edit/:id', to: 'admins#edit'
+  # end
   namespace :site do
     get 'welcome/index'
   end
@@ -9,7 +10,8 @@ Rails.application.routes.draw do
     get 'welcome/index'
   end
   namespace :admins_backoffice do
-    get 'welcome/index'
+    get 'welcome/index' # dashboard
+    resources :admins, only: [:index] # administradores
   end
   devise_for :users
   devise_for :admins
