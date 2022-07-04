66df839be49e4e270703d6727cc6f732f1d8f223
Matheus Guerreiro <73855206+matheusguerreiro@users.noreply.github.com>
2022-07-03 21:02:46 -0300

    s5 a172. Problema N+1 do SQL_Active Record

--- section 5: segunda aplicação (aprofundando-se)/time_to_answer/app/controllers/admins_backoffice/questions_controller.rb
+++ section 5: segunda aplicação (aprofundando-se)/time_to_answer/app/controllers/admins_backoffice/questions_controller.rb
@@ -4,7 +4,7 @@ class AdminsBackoffice::QuestionsController < AdminsBackofficeController
     
 
   def index
-    @questions = Question.all.order(:description).page(params[:page])
+    @questions = Question.includes(:subject).order(:description).page(params[:page])
   end
 
   def new
