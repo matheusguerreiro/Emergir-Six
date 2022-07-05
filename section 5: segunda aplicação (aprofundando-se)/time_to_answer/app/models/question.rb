56b01e654b6d22b8364bf298bca0dbedcc21bd15
Matheus Guerreiro <73855206+matheusguerreiro@users.noreply.github.com>
2022-07-05 18:09:11 -0300

    s5 a197. Usando Scopes

--- section 5: segunda aplicação (aprofundando-se)/time_to_answer/app/models/question.rb
+++ section 5: segunda aplicação (aprofundando-se)/time_to_answer/app/models/question.rb
@@ -5,15 +5,25 @@ class Question < ApplicationRecord
 
   paginates_per 5
 
-  def self.search(page, term)
+  # def self.search(page, term)
+  #   Question.includes(:answers)
+  #           .where("lower(description) LIKE ?", "%#{term.downcase}%")
+  #           .page(page)
+  # end
+
+  scope :_search_, -> (page, term) {
     Question.includes(:answers)
             .where("lower(description) LIKE ?", "%#{term.downcase}%")
             .page(page)
-  end
+  }
 
   def self.last_questions(page)
-    Question.includes(:answers)
-      .order("created_at desc")
-      .page(page)
+    includes(:answers)
+    .order("created_at desc")
+    .page(page)
   end
+
+  # obs: como self é = a Question, podemos tirar o Question fora. exemplo: self.last_questions(page)
+  # obs: podemos transformar para scope ao invés de self.search(page, term), somente porque estamos fazendo pesquisa, caso contrário usamos o self.nome(..., ...)
+
 end
