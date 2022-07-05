d0be1eac1d53183cb7216cdae3ae166d78fb1c52
Matheus Guerreiro <73855206+matheusguerreiro@users.noreply.github.com>
2022-07-05 17:47:45 -0300

    s5 a196. Usando métodos de classe no model

--- section 5: segunda aplicação (aprofundando-se)/time_to_answer/app/controllers/site/search_controller.rb
+++ section 5: segunda aplicação (aprofundando-se)/time_to_answer/app/controllers/site/search_controller.rb
@@ -2,8 +2,6 @@ class Site::SearchController < SiteController
   def questions
     # params[:term]
     # @questions = Question.includes(:answers)
-    @questions = Question.includes(:answers)
-                         .where("lower(description) LIKE ?", "%#{params[:term].downcase}%")
-                         .page(params[:page])
+    @questions = Question.search(params[:page], params[:term])
   end
 end
