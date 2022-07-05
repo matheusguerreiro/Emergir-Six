c8244ad99a1bc78484e356d501ccdce49177004c
Matheus Guerreiro <73855206+matheusguerreiro@users.noreply.github.com>
2022-07-05 17:16:18 -0300

    s5 a194. Fazendo a pesquisa funcionar

--- section 5: segunda aplicação (aprofundando-se)/time_to_answer/app/controllers/site/search_controller.rb
+++ section 5: segunda aplicação (aprofundando-se)/time_to_answer/app/controllers/site/search_controller.rb
@@ -1,5 +1,9 @@
 class Site::SearchController < SiteController
   def questions
+    # params[:term]
+    # @questions = Question.includes(:answers)
     @questions = Question.includes(:answers)
+                         .where("lower(description) LIKE ?", "%#{params[:term].downcase}%")
+                         .page(params[:page])
   end
 end
