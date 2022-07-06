59866fd22eec7960a02546db5f0f6546d146bdc1
Matheus Guerreiro <73855206+matheusguerreiro@users.noreply.github.com>
2022-07-06 16:55:12 -0300

    s5 a204. Filtrando as questões por assunto

--- section 5: segunda aplicação (aprofundando-se)/time_to_answer/app/controllers/site/search_controller.rb
+++ section 5: segunda aplicação (aprofundando-se)/time_to_answer/app/controllers/site/search_controller.rb
@@ -4,4 +4,9 @@ class Site::SearchController < SiteController
     # @questions = Question.includes(:answers)
     @questions = Question._search_(params[:page], params[:term])
   end
+
+  def subject
+    @questions = Question._search_subject_(params[:page], params[:subject_id])
+    
+  end
 end
--- section 5: segunda aplicação (aprofundando-se)/time_to_answer/app/models/question.rb
+++ section 5: segunda aplicação (aprofundando-se)/time_to_answer/app/models/question.rb
@@ -12,16 +12,22 @@ class Question < ApplicationRecord
   # end
 
   scope :_search_, -> (page, term) {
-    Question.includes(:answers)
+    Question.includes(:answers, :subject)
             .where("lower(description) LIKE ?", "%#{term.downcase}%")
             .page(page)
   }
 
   def self.last_questions(page)
-    includes(:answers)
+    includes(:answers, :subject)
     .order("created_at desc")
     .page(page)
   end
+
+  scope :_search_subject_, -> (page, subject_id) {
+    includes(:answers, :subject)
+    .where(subject_id: subject_id)
+    .page(page)
+  }
 
   # obs: como self é = a Question, podemos tirar o Question fora. exemplo: self.last_questions(page)
   # obs: podemos transformar para scope ao invés de self.search(page, term), somente porque estamos fazendo pesquisa, caso contrário usamos o self.nome(..., ...)
