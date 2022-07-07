f2073870391a9fc986244d6b0d67f9b966a7e7f8
Matheus Guerreiro <73855206+matheusguerreiro@users.noreply.github.com>
2022-07-07 10:13:19 -0300

    s5 a215. Trabalhando com o radiobutton

--- section 5: segunda aplicação (aprofundando-se)/time_to_answer/app/models/user.rb
+++ section 5: segunda aplicação (aprofundando-se)/time_to_answer/app/models/user.rb
@@ -4,8 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one :user_profile
+  has_one :user_profile, :dependent => :destroy
   accepts_nested_attributes_for :user_profile, reject_if: :all_blank
+  # , allow_destroy: true
   # user vai aceitar atributos da tabela user_profile
 
   validates :first_name, presence: true, length: {minimum:3}, on: :update
