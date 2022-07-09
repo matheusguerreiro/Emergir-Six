43152eaf5ee92dd5548b16167daa5f3d32a49d29
Matheus Guerreiro <73855206+matheusguerreiro@users.noreply.github.com>
2022-07-08 22:06:52 -0300

    s5 a232. Conhecendo o Action Mailer

--- section 5: segunda aplicação (aprofundando-se)/time_to_answer/app/controllers/admins_backoffice/admins_controller.rb
+++ section 5: segunda aplicação (aprofundando-se)/time_to_answer/app/controllers/admins_backoffice/admins_controller.rb
@@ -11,6 +11,7 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
 
   def update
     if @admin.update(params_admin)
+      AdminMailer.update_email(current_admin, @admin).deliver_now
       redirect_to admins_backoffice_admins_path, notice: "admin Atualizado!"
     else
       render :edit
